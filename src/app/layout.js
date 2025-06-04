65110930f19bd31ede439ebe108333965410924e
fabrazzak <freelancerabdurrazzak47@gmail.com>
2025-06-05 00:45:16 +0600

    update code

--- src/app/layout.js
+++ src/app/layout.js
@@ -33,6 +33,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <meta name="google-adsense-account" content="ca-pub-7987554086610229"></meta>
         <Script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7987554086610229"
