957ab7cbc77747949ac24d4103c1513dbc843bce
fabrazzak <freelancerabdurrazzak47@gmail.com>
2025-06-08 11:44:13 +0600

    update code

--- src/components/AdBanner/AdBanner.jsx
+++ src/components/AdBanner/AdBanner.jsx
@@ -1,36 +1,30 @@
 import { useEffect, useRef } from "react";
-import { motion } from "framer-motion";
 
 const AdBanner = () => {
   const adRef = useRef(null);
 
   useEffect(() => {
-    if (typeof window === 'undefined') return;
+    if (typeof window === "undefined") return;
 
-    const adElement = adRef.current;
-
-    // Check if the ad is already initialized
-    if (!adElement?.getAttribute('data-adsbygoogle-status')) {
-      try {
+    try {
+      if (window.adsbygoogle && adRef.current) {
         (window.adsbygoogle = window.adsbygoogle || []).push({});
-      } catch (e) {
-        console.error('Adsense error:', e);
       }
+    } catch (e) {
+      console.error("Adsense error:", e);
     }
   }, []);
 
-  return (   
-
-      <ins
-        className="adsbygoogle"
-        style={{ display: 'block' }}
-        data-ad-client="ca-pub-7987554086610229"
-        data-ad-slot="5903461119"
-        data-ad-format="auto"
-        data-full-width-responsive="true"
-        ref={adRef}
-      ></ins>
-   
+  return (
+    <ins
+      className="adsbygoogle"
+      style={{ display: "block" }}
+      data-ad-client="ca-pub-7987554086610229"
+      data-ad-slot="5903461119"
+      data-ad-format="auto"
+      data-full-width-responsive="true"
+      ref={adRef}
+    />
   );
 };
 
