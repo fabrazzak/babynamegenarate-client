5dcf135f0a094dbff8096ef78cde8b603381bd86
fabrazzak <freelancerabdurrazzak47@gmail.com>
2025-06-08 11:53:30 +0600

    upate

--- src/components/AdBanner/AdBanner.jsx
+++ src/components/AdBanner/AdBanner.jsx
@@ -3,28 +3,30 @@ import { useEffect, useRef } from "react";
 const AdBanner = () => {
   const adRef = useRef(null);
 
-  useEffect(() => {
-    if (typeof window === "undefined") return;
+  // useEffect(() => {
+  //   if (typeof window === "undefined") return;
 
-    try {
-      if (window.adsbygoogle && adRef.current) {
-        (window.adsbygoogle = window.adsbygoogle || []).push({});
-      }
-    } catch (e) {
-      console.error("Adsense error:", e);
-    }
-  }, []);
+  //   try {
+  //     if (window.adsbygoogle && adRef.current) {
+  //       (window.adsbygoogle = window.adsbygoogle || []).push({});
+  //     }
+  //   } catch (e) {
+  //     console.error("Adsense error:", e);
+  //   }
+  // }, []);
 
   return (
-    <ins
-      className="adsbygoogle"
-      style={{ display: "block" }}
-      data-ad-client="ca-pub-7987554086610229"
-      data-ad-slot="5903461119"
-      data-ad-format="auto"
-      data-full-width-responsive="true"
-      ref={adRef}
-    />
+    <>
+      <ins class="adsbygoogle"
+        style="display:block"
+        data-ad-client="ca-pub-7987554086610229"
+        data-ad-slot="5903461119"
+        data-ad-format="auto"
+        data-full-width-responsive="true"></ins>
+      <script>
+        (adsbygoogle = window.adsbygoogle || []).push({ });
+      </script>
+      </>
   );
 };
 
